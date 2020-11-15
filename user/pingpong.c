43184decd5ebef80a9ed150bece1fd262899b33b
Comzyh <comzyh@gmail.com>
2020-11-15 14:31:09 +0800

    fix pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,20 +5,29 @@
 int
 main(int argc, char* argv[])
 {
-  int p[2];
-  pipe(p);
+  int p2c[2], c2p[2];
+  pipe(p2c);
+  pipe(c2p);
   if(fork() > 0) { // parent
-    uint8 byte_parent = 0;
-    write(p[0], &byte_parent, 1);
-    read(p[0], &byte_parent, 1);
+    uint8 byte_parent = 123;
+    if(write(p2c[1], &byte_parent, 1) != 1) {
+      fprintf(2, "error write ping\n");
+    }
+    if(read(c2p[0], &byte_parent, 1) != 1) {
+      fprintf(2, "error read pong\n");
+    }
     int pid = getpid();
     printf("%d: received pong\n", pid);
   } else { // child
     uint8 byte_child;
-    read(p[1], &byte_child, 1);
+    if(read(p2c[0], &byte_child, 1) != 1) {
+      fprintf(2, "error read ping\n");
+    }
     int pid = getpid();
     printf("%d: received ping\n", pid);
-    write(p[1], &byte_child, 1);
+    if(write(c2p[1], &byte_child, 1) != 1) {
+      fprintf(2, "error write pong\n");
+    }
   }
   exit(0);
 }
